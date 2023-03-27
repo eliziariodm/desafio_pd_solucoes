545efd1ee4fe36b8227265ee0a6a09d97fd30b23
Daniel Eliziario <55812025+eliziariodm@users.noreply.github.com>
2023-03-27 10:23:51 -0300

    latest fixes

--- lib/app/modules/squads/widgets/container_date_widget.dart
+++ lib/app/modules/squads/widgets/container_date_widget.dart
@@ -15,94 +15,102 @@ class ContainerDateWidget extends StatelessWidget {
 
     return Consumer<SquadsController>(
       builder: (_, squads, __) {
-        return Column(
-          children: [
-            const SizedBox(height: 32),
-            squads.auxReportsList.isEmpty
-                ? squads.isWarning
-                    ? const SizedBox.shrink()
-                    : UiValidateContainer(
-                        width: 400,
-                        textValidate: 'Lance suas horas antes de filtrá-las.',
-                        onTap: () {
-                          squads.onClosedSquads();
-                        },
-                      )
-                : DataTable(
-                    columnSpacing: screen.isMobile(context) ? 10 : 130,
-                    horizontalMargin: screen.isMobile(context) ? 10 : 30,
-                    headingTextStyle: TextStyle(
-                      fontSize: screen.isMobile(context) ? 12 : 16,
-                      color: const Color(0xFFFFFFFF),
-                      fontWeight: FontWeight.w700,
-                      fontFamily: 'Roboto',
-                    ),
-                    dataTextStyle: TextStyle(
-                      fontSize: screen.isMobile(context) ? 12 : 16,
-                      color: const Color(0xFF000000),
-                      fontWeight: FontWeight.w400,
-                      fontFamily: 'Roboto',
-                    ),
-                    columns: const [
-                      DataColumn(label: Text('Membro')),
-                      DataColumn(label: Text('Descrição')),
-                      DataColumn(label: Text('Horas')),
-                      DataColumn(label: Text('Criado em')),
-                    ],
-                    rows: List<DataRow>.generate(
-                      squads.auxReportsList.length,
-                      (index) {
-                        var squadsDate = DateTime.parse(
-                            squads.auxReportsList[index].createdAt);
-
-                        var squadsStringDate =
-                            DateFormat('dd/MM/yyyy').format(squadsDate);
-
-                        return DataRow(
-                          cells: [
-                            DataCell(
-                              Text(squads.auxEmployeesList[index].name),
-                            ),
-                            DataCell(
-                              Text(squads.auxReportsList[index].description),
-                            ),
-                            DataCell(
-                              Text(squads.auxReportsList[index].spentHours
-                                  .toString()),
-                            ),
-                            DataCell(
-                              Text(squadsStringDate),
-                            ),
+        return SizedBox(
+          height: screen.isMobile(context) ? 590 : 600,
+          child: Column(
+            children: [
+              const SizedBox(height: 32),
+              squads.auxReportsList.isEmpty
+                  ? squads.isWarning
+                      ? const SizedBox.shrink()
+                      : UiValidateContainer(
+                          width: 400,
+                          textValidate: 'Lance suas horas antes de filtrá-las.',
+                          onTap: () {
+                            squads.onClosedSquads();
+                          },
+                        )
+                  : Expanded(
+                      child: SingleChildScrollView(
+                        child: DataTable(
+                          columnSpacing: screen.isMobile(context) ? 10 : 130,
+                          horizontalMargin: screen.isMobile(context) ? 10 : 30,
+                          headingTextStyle: TextStyle(
+                            fontSize: screen.isMobile(context) ? 12 : 16,
+                            color: const Color(0xFFFFFFFF),
+                            fontWeight: FontWeight.w700,
+                            fontFamily: 'Roboto',
+                          ),
+                          dataTextStyle: TextStyle(
+                            fontSize: screen.isMobile(context) ? 12 : 16,
+                            color: const Color(0xFF000000),
+                            fontWeight: FontWeight.w400,
+                            fontFamily: 'Roboto',
+                          ),
+                          columns: const [
+                            DataColumn(label: Text('Membro')),
+                            DataColumn(label: Text('Descrição')),
+                            DataColumn(label: Text('Horas')),
+                            DataColumn(label: Text('Criado em')),
                           ],
-                        );
-                      },
+                          rows: List<DataRow>.generate(
+                            squads.auxReportsList.length,
+                            (index) {
+                              var squadsDate = DateTime.parse(
+                                  squads.auxReportsList[index].createdAt);
+
+                              var squadsStringDate =
+                                  DateFormat('dd/MM/yyyy').format(squadsDate);
+
+                              return DataRow(
+                                cells: [
+                                  DataCell(
+                                    Text(squads.auxEmployeesList[index].name),
+                                  ),
+                                  DataCell(
+                                    Text(squads
+                                        .auxReportsList[index].description),
+                                  ),
+                                  DataCell(
+                                    Text(squads.auxReportsList[index].spentHours
+                                        .toString()),
+                                  ),
+                                  DataCell(
+                                    Text(squadsStringDate),
+                                  ),
+                                ],
+                              );
+                            },
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
-            const SizedBox(height: 32),
-            Text(
-              'Horas totais da squad',
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            const SizedBox(height: 32),
-            Text(
-              '${squads.sum} Horas',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 32),
-            Text(
-              'Média de horas por dia',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.displayLarge,
-            ),
-            const SizedBox(height: 32),
-            Text(
-              '${squads.average.round()} Horas/Dia',
-              textAlign: TextAlign.center,
-              style: Theme.of(context).textTheme.bodyLarge,
-            ),
-            const SizedBox(height: 34),
-          ],
+              const SizedBox(height: 32),
+              Text(
+                'Horas totais da squad',
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(height: 32),
+              Text(
+                '${squads.sum} Horas',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 32),
+              Text(
+                'Média de horas por dia',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.displayLarge,
+              ),
+              const SizedBox(height: 32),
+              Text(
+                '${squads.average.toStringAsFixed(1)} Horas/Dia',
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyLarge,
+              ),
+              const SizedBox(height: 34),
+            ],
+          ),
         );
       },
     );
